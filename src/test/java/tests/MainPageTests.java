d5b2ab271d645e2e9f122ac3780f8452cbc17a7b
nyash <alex-nyashin1996@yandex.ru>
2023-01-07 14:57:36 +0300

    add new project

--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
 
@@ -7,9 +9,32 @@ public class MainPageTests {
 
     MainPage mainPage = new MainPage();
 
+    @BeforeAll
+    static void setUp() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+
+//        String baseUrl = "https://demoqa.com";
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browser_version", "100.0");
+        String browserSize = System.getProperty("browser_size", "1920x1080");
+        String remote = System.getProperty("remote");
+
+//        Configuration.baseUrl = baseUrl;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.browserSize = browserSize;
+
+        if (remote != null) {
+            Configuration.remote = remote;
+        }
+    }
+
     @Test
     void searchVacancy() {
         mainPage.searchVacancy();
-
     }
 }
